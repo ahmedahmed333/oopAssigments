632e940a2ce17e8f9b21573fafbe3211246523dc
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-09-03 02:47:40 +0300

    Add project files.

---
+++ oopAssigments/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace oopAssigments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
