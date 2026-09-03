62ddd4847dd246db13d165cc2665524e148f875f
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-09-03 03:09:46 +0300

    Assignment02 OOP Question 1 (b)

--- oopAssigments/Program.cs
+++ oopAssigments/Program.cs
@@ -9,6 +9,11 @@
             // A class is refrance type , while a struct s a value type. classes are generally used to represent complex objects , while structs are typically used for small and smple data types.
             // classes support inhertance and polymorphism, whreas structs do not support inheritance. 
 
+            //(b)
+
+            // Classes are more sutable for large applications becuase they are designed to represent complex objects with data and behanior.
+            // They support important OOP concepts such as inheritance, polymorphism, and encapsulation, which make the code more flexible, maintainable.
+
             #endregion
         }
     }
